a32882f1c07a52010cc65b951248ccd47ffc536b
Junel Maputol Maglunsod <42353316+along4short@users.noreply.github.com>
2023-07-24 20:35:38 +0800

    Add files via upload

--- masterSocketNode.js
+++ masterSocketNode.js
@@ -3,12 +3,12 @@ const socketIO = require('socket.io');
 const PORT = 3000; // Change this to your desired port number
 
 const acceptedWebApps = [
-  { domain: "google.com" },
-  { domain: "test.com" },
+  { domain: "google.com", backend: "http://google.com" },
+  { domain: "test.com", backend: "http://2.2.2.2" },
   // Add other accepted web applications here
 ];
 
-const server = socketIO(PORT); // Use socketIO here
+const server = socketIO(PORT);
 
 server.on('connection', (socket) => {
   console.log('Reverse proxy server connected.');
@@ -17,10 +17,27 @@ server.on('connection', (socket) => {
   socket.emit('data', JSON.stringify(acceptedWebApps));
 
   socket.on('data', (data) => {
-    // Handle data received from the reverse proxy server if needed
-    // For example, you can parse the data as JSON and perform actions accordingly
-    const message = data.toString().trim();
-    console.log('Received data from reverse proxy:', message);
+    try {
+      // Handle data received from the reverse proxy server if needed
+      const message = JSON.parse(data);
+      // Add necessary validation and authentication for the received data
+      if (!Array.isArray(message)) {
+        throw new Error('Invalid data format received from reverse proxy. Expected an array.');
+      }
+      for (const app of message) {
+        if (!app.domain || !isValidDomain(app.domain) || !app.backend) {
+          throw new Error('Invalid data format received from reverse proxy. Each object should have "domain" and "backend" properties.');
+        }
+      }
+      console.log('Received data from reverse proxy:', message);
+
+      // Update the accepted web applications list based on the received data
+      for (const app of message) {
+        acceptedWebApps.push(app);
+      }
+    } catch (error) {
+      console.error('Error processing data from reverse proxy:', error.message);
+    }
   });
 
   socket.on('disconnect', () => {
