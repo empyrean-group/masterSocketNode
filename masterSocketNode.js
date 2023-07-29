259976c14c2cc328b4b20b7c118fffb0d0e2ed94
Junel Maputol Maglunsod <42353316+along4short@users.noreply.github.com>
2023-07-29 21:24:30 +0800

    Add files via upload

--- masterSocketNode.js
+++ masterSocketNode.js
@@ -1,6 +1,9 @@
 const socketIO = require('socket.io');
+const redis = require('redis');
 
 const PORT = 3000; // Change this to your desired port number
+const BLOCK_THRESHOLD = 100; // Number of connections per second threshold for blocking an IP
+const BLOCK_EXPIRY = 300; // Expiry time for blocked IPs in seconds (5 minutes)
 
 const acceptedWebApps = [
   { domain: "google.com", backends: ["http://google.com"] },
@@ -10,16 +13,29 @@ const acceptedWebApps = [
 
 const server = socketIO(PORT);
 
+// Redis configuration
+const redisClient = redis.createClient();
+
+// Function to validate and sanitize the input
+const isValidDomain = (domain) => {
+  // Add more validation as per your requirements
+  return /^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(domain);
+};
+
 server.on('connection', (socket) => {
   console.log('Reverse proxy server connected.');
 
   // Send the list of accepted web applications when a reverse proxy server connects
-  socket.emit('data', JSON.stringify(acceptedWebApps));
+  socket.emit('data', acceptedWebApps);
 
   socket.on('data', (data) => {
     try {
       // Handle data received from the reverse proxy server if needed
-      const message = JSON.parse(data);
+      const message = Array.isArray(data) ? data : data;
+
+      // Log the received message for debugging
+      console.log('Received message from reverse proxy:', message);
+
       // Add necessary validation and authentication for the received data
       if (!Array.isArray(message)) {
         throw new Error('Invalid data format received from reverse proxy. Expected an array.');
@@ -37,6 +53,31 @@ server.on('connection', (socket) => {
       }
     } catch (error) {
       console.error('Error processing data from reverse proxy:', error.message);
+    }
+  });
+
+  // Add an event listener for data from child socket nodes
+  socket.on('childData', (data) => {
+    try {
+      // Assuming the data received contains the IP and connectionsPerSecond information
+      const { ip, connectionsPerSecond } = typeof data === 'string' ? JSON.parse(data) : data;
+
+      // Log the received data for monitoring purposes
+      console.log(`Received connections per second from IP ${ip}: ${connectionsPerSecond}`);
+
+      // Check if the number of connections per second exceeds the block threshold
+      if (connectionsPerSecond > BLOCK_THRESHOLD) {
+        // Block the IP using Redis with an expiry time of 5 minutes
+        redisClient.setex(`blocked:${ip}`, BLOCK_EXPIRY, 'true', (err) => {
+          if (err) {
+            console.error(`Error blocking IP ${ip} in Redis:`, err.message);
+          } else {
+            console.log(`Blocked IP ${ip} for ${BLOCK_EXPIRY} seconds.`);
+          }
+        });
+      }
+    } catch (error) {
+      console.error('Error processing data from child node:', error.message);
     }
   });
 
