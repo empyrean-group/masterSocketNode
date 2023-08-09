2ac792fcec7476f4a85c937d1b81814bfca6b4d6
Junel Maputol Maglunsod <42353316+along4short@users.noreply.github.com>
2023-08-09 22:39:18 +0800

    Add files via upload

--- masterSocketNode.js
+++ masterSocketNode.js
@@ -1,5 +1,6 @@
 const socketIO = require('socket.io');
 const redis = require('redis');
+const MongoClient = require('mongodb').MongoClient;
 
 const PORT = 3000; // Change this to your desired port number
 const BLOCK_THRESHOLD = 100; // Number of connections per second threshold for blocking an IP
@@ -16,108 +17,129 @@ const server = socketIO(PORT);
 // Redis configuration
 const redisClient = redis.createClient();
 
+// MongoDB configuration
+const mongoUrl = 'mongodb://localhost:27017';
+const dbName = 'connection_tracking_db'; // Change this to your desired database name
+
+MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err.message);
+    return;
+  }
+  const db = client.db(dbName);
+
+  // Socket.io server
+  server.on('connection', (socket) => {
+    console.log('Reverse proxy server connected.');
+
+    // Send the list of accepted web applications when a reverse proxy server connects
+    socket.emit('data', JSON.stringify(acceptedWebApps));
+
+    let systemInfoReceived = false;
+    let systemInfoData = '';
+
+    socket.on('data', (data) => {
+      try {
+        // Handle data received from the reverse proxy server if needed
+        if (!systemInfoReceived) {
+          // Check if the data is already parsed JSON or a string
+          const newData = typeof data === 'object' ? data : JSON.parse(data);
+          systemInfoData += JSON.stringify(newData); // Add to the systemInfoData string
+
+          // Check if the systemInfoData contains the "systemInfo" property
+          const parsedData = JSON.parse(systemInfoData);
+          if (parsedData && parsedData.systemInfo) {
+            console.log('Received systemInfo from reverse proxy:', JSON.stringify(parsedData.systemInfo, null, 2));
+            // Process systemInfo data here if needed
+            systemInfoReceived = true;
+            return;
+          }
+        }
+
+        // Assuming the data is an array of accepted web applications
+        const parsedData = JSON.parse(data);
+        console.log('Received data from reverse proxy:', parsedData);
+
+        // Add necessary validation and authentication for the received data
+        if (!Array.isArray(parsedData)) {
+          throw new Error('Invalid data format received from reverse proxy. Expected an array.');
+        }
+        for (const app of parsedData) {
+          if (!app.domain || !isValidDomain(app.domain) || !app.backends || !Array.isArray(app.backends)) {
+            throw new Error('Invalid data format received from reverse proxy. Each object should have "domain" and "backends" properties, where "backends" is an array of backend server URLs.');
+          }
+        }
+        console.log('Parsed data from reverse proxy:', parsedData);
+
+        // Update the accepted web applications list based on the received data
+        for (const app of parsedData) {
+          acceptedWebApps.push(app);
+        }
+      } catch (error) {
+        console.error('Error processing data from reverse proxy:', error.message);
+      }
+    });
+
+    socket.on('childData', (data) => {
+      try {
+        // Assuming the data received contains the IP and connectionsPerSecond information
+        const { ip, connectionsPerSecond } = typeof data === 'string' ? JSON.parse(data) : data;
+
+        // Log the received data for monitoring purposes
+        console.log(`Received connections per second from IP ${ip}: ${connectionsPerSecond}`);
+
+        // Check if the number of connections per second exceeds the block threshold
+        if (connectionsPerSecond > BLOCK_THRESHOLD) {
+          // Block the IP using Redis with an expiry time of 5 minutes
+          redisClient.setex(`blocked:${ip}`, BLOCK_EXPIRY, 'true', (err) => {
+            if (err) {
+              console.error(`Error blocking IP ${ip} in Redis:`, err.message);
+            } else {
+              console.log(`Blocked IP ${ip} for ${BLOCK_EXPIRY} seconds.`);
+              // Broadcast the blocked IP to all child nodes
+              server.emit('blockedIP', JSON.stringify(ip));
+            }
+          });
+        }
+
+        // Update MongoDB with the connection data
+        const collection = db.collection('connection_data');
+        const timestamp = new Date();
+        collection.insertOne({ ip, timestamp, count: connectionsPerSecond });
+
+      } catch (error) {
+        console.error('Error processing data from child node:', error.message);
+      }
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Reverse proxy server disconnected.');
+      // Optionally, you may want to gracefully shutdown the master node when the reverse proxy disconnects
+      server.close(() => {
+        console.log('Master socket node shut down gracefully.');
+      });
+    });
+
+    socket.on('error', (err) => {
+      console.error('Socket error:', err.message);
+    });
+  });
+
+  console.log(`Master socket node started and listening on port ${PORT}`);
+
+  // Clean up the Redis and MongoDB connections when the master node is stopped
+  process.on('SIGINT', () => {
+    redisClient.quit(() => {
+      console.log('Redis connection closed.');
+      client.close(); // Close MongoDB connection
+      process.exit(0);
+    });
+  });
+
+});
+
 // Function to validate and sanitize the input
 const isValidDomain = (domain) => {
   // Add more validation as per your requirements
   return /^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(domain);
 };
-
-server.on('connection', (socket) => {
-  console.log('Reverse proxy server connected.');
-
-  // Send the list of accepted web applications when a reverse proxy server connects
-  socket.emit('data', JSON.stringify(acceptedWebApps));
-
-  let systemInfoReceived = false;
-  let systemInfoData = '';
-
-  socket.on('data', (data) => {
-    try {
-      // Handle data received from the reverse proxy server if needed
-      if (!systemInfoReceived) {
-        // Check if the data is already parsed JSON or a string
-        const newData = typeof data === 'object' ? data : JSON.parse(data);
-        systemInfoData += JSON.stringify(newData); // Add to the systemInfoData string
-
-        // Check if the systemInfoData contains the "systemInfo" property
-        const parsedData = JSON.parse(systemInfoData);
-        if (parsedData && parsedData.systemInfo) {
-          console.log('Received systemInfo from reverse proxy:', JSON.stringify(parsedData.systemInfo, null, 2));
-          // Process systemInfo data here if needed
-          systemInfoReceived = true;
-          return;
-        }
-      }
-
-      // Assuming the data is an array of accepted web applications
-      const parsedData = JSON.parse(data);
-      console.log('Received data from reverse proxy:', parsedData);
-
-      // Add necessary validation and authentication for the received data
-      if (!Array.isArray(parsedData)) {
-        throw new Error('Invalid data format received from reverse proxy. Expected an array.');
-      }
-      for (const app of parsedData) {
-        if (!app.domain || !isValidDomain(app.domain) || !app.backends || !Array.isArray(app.backends)) {
-          throw new Error('Invalid data format received from reverse proxy. Each object should have "domain" and "backends" properties, where "backends" is an array of backend server URLs.');
-        }
-      }
-      console.log('Parsed data from reverse proxy:', parsedData);
-
-      // Update the accepted web applications list based on the received data
-      for (const app of parsedData) {
-        acceptedWebApps.push(app);
-      }
-    } catch (error) {
-      console.error('Error processing data from reverse proxy:', error.message);
-    }
-  });
-
-  socket.on('childData', (data) => {
-    try {
-      // Assuming the data received contains the IP and connectionsPerSecond information
-      const { ip, connectionsPerSecond } = typeof data === 'string' ? JSON.parse(data) : data;
-
-      // Log the received data for monitoring purposes
-      console.log(`Received connections per second from IP ${ip}: ${connectionsPerSecond}`);
-
-      // Check if the number of connections per second exceeds the block threshold
-      if (connectionsPerSecond > BLOCK_THRESHOLD) {
-        // Block the IP using Redis with an expiry time of 5 minutes
-        redisClient.setex(`blocked:${ip}`, BLOCK_EXPIRY, 'true', (err) => {
-          if (err) {
-            console.error(`Error blocking IP ${ip} in Redis:`, err.message);
-          } else {
-            console.log(`Blocked IP ${ip} for ${BLOCK_EXPIRY} seconds.`);
-            // Broadcast the blocked IP to all child nodes
-            server.emit('blockedIP', JSON.stringify(ip));
-          }
-        });
-      }
-    } catch (error) {
-      console.error('Error processing data from child node:', error.message);
-    }
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Reverse proxy server disconnected.');
-    // Optionally, you may want to gracefully shutdown the master node when the reverse proxy disconnects
-    server.close(() => {
-      console.log('Master socket node shut down gracefully.');
-    });
-  });
-
-  socket.on('error', (err) => {
-    console.error('Socket error:', err.message);
-  });
-});
-
-console.log(`Master socket node started and listening on port ${PORT}`);
-
-// Clean up the Redis connection when the master node is stopped
-process.on('SIGINT', () => {
-  redisClient.quit(() => {
-    console.log('Redis connection closed.');
-    process.exit(0);
-  });
-});
